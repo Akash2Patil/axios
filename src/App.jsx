32a6ed0ec365f3b181a017cfb5b2774aa4c0bd4f
Akash Patil <akashpatil1321999@gmail.com>
2025-03-07 23:13:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [products, setProducts] = useState([]); // State to store API data
+  const [loading, setLoading] = useState(true); // State to handle loading
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        console.log(response);
+        
+        setProducts(response.data); // Store API response in state
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="w-full h-auto bg-zinc-900 text-white p-5">
+      <h1 className="text-2xl font-bold mb-4">Hi, my name is Akash Patil</h1>
+      
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {products.map((product) => (
+            <div key={product.id} className="p-3 border border-gray-700 mb-2">
+              <img src={product.image} alt="" />
+              <h2 className="text-xl">{product.title}</h2>
+              <p className="text-gray-400">${product.price}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
