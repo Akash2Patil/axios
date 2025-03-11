802ff448bae04892c0868d1e18ef7dac8ead0102
Akash Patil <akashpatil1321999@gmail.com>
2025-03-11 23:06:40 +0530

    products are listing now usimg api and shown using state hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const[products,setproducts]= useState([])
   const api = "https://fakestoreapi.com/products";
   const getproducts = () => {
 
     axios.get(api)
       .then((response) => {
-        console.log(response);
+        // console.log(response);
+        setproducts(response.data)
       })
       .catch((err) => {
         console.log(err);
@@ -27,15 +29,21 @@ const App = () => {
 
     }).catch(err => console.log(err));
   }
-
+  console.log({products});
+  
 return (
   <div className='px-10 py-20 w-full h-screen'>
-    <button onClick={getproducts} className='bg-red-500 px-4 py-3 text-white rounded'>
+    <button onClick={getproducts} className='bg-red-500 px-4 py-3 text-white rounded mr-3'>
       get products
     </button>
   <button onClick={addproducts} className='bg-green-500 px-4 py-3 text-white rounded'>
         add products
   </button>
+  <br /><br />
+  <ul>
+    {products.length > 0 ? products.map(pro => <li className='bg-red-500 w-full py-3 px-5 text-white mb-2'>{pro.title}</li>) : <h1>loading...</h1> }
+    
+  </ul>
   </div>
 )
 }
